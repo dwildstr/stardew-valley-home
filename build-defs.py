19ce2691ef1eb576dbe7da1ce3e42ce6fdc5439f
Jake Wildstrom <dwildstr@gmail.com>
2025-08-16 13:31:51 -0400

    Initial commit

---
+++ build-defs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import base64
+from PIL import Image
+import fileinput
+from io import BytesIO
+
+def getBase64(pngfile,x,y,width,height):
+    im=Image.open(pngfile)
+    buffered=BytesIO()
+    im.crop((x,y,x+width,y+height)).save(buffered,format="PNG")
+    return base64.b64encode(buffered.getvalue()).decode('ascii')
+
+print('<defs>')
+blockSize=1
+for line in fileinput.input():
+    if line[0]=="#":
+        continue
+    if line[0]=="%":
+        blockSize=int(line.removeprefix("%"))
+    line=line.rstrip()
+    params=line.split(" ")
+    if len(params)<6:
+        continue
+    b64string = getBase64(params[0],int(params[1])*blockSize,int(params[2])*blockSize,int(params[3])*blockSize,int(params[4])*blockSize)
+    print('  <g id ="'+params[5]+'">')
+    print('    <image x="0" y="0" width="'+str(int(params[3])*blockSize)+'" height="'+str(int(params[4])*blockSize)+'" href="data:image/png;base64,'+b64string+'"/>')
+    print('  </g>')
+print('</defs>')
